27ad8386f75b6d88e3a156fb0904ed5597a29753
Djenane Yassine <djenane.yassine1@gmail.com>
2024-02-22 15:11:08 +0100

    🐛 FIX: Modif conditions "conditionsTestPassed"  -> push les scores inf à 9 et 10, et fix des scores push aléatoirement à la BDD sup à 4 point d'écart.

--- src/Controller/StatistiquesController.php
+++ src/Controller/StatistiquesController.php
@@ -7,7 +7,6 @@ use App\Services\CallApiService;
 use App\Services\StatsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
